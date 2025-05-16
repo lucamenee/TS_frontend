9d3ffff7c9d784b8b4a672d859a2546e0f511011
Luca Meneghetti <126782219+lucamenee@users.noreply.github.com>
2025-05-16 11:30:08 +0200

    delete users

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,32 +1,85 @@
 import { Component } from '@angular/core';
 import { UserHttpService } from '../user-http.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../my_types/User';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
 
-  public errmessage = undefined;
+  public airlineerrmessage = undefined;
+  public deleteusererrmessage = undefined;
+  public users!: Observable<User[]>;
+  selectedUser: string = '';
 
-  constructor(private http: HttpClient, private us: UserHttpService) {}
+  passwordText: string = '';  
+  emailText: string = '';
+  usernameText: string = '';
+  nameText: string = '';
+
+  constructor(private http: HttpClient, private us: UserHttpService) {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.users = this.http.get<User[]>(this.us.url + '/users', {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + this.us.get_token(),
+        'cache-control': 'no-cache',
+        'Content-Type':  'application/json',
+      })
+    })
+  }
 
   public createAirline(username: string, name: string, password: string, email: string) {
     
     this.us.register(username, name, password, email, 'AIRLINE', '').subscribe({
       next: (d) => {
-        this.errmessage = undefined;
+        this.airlineerrmessage = undefined;
         alert('Utente creato con successo');
-
+        this.getUsers();
+        this.passwordText = '';
+        this.emailText = '';
+        this.usernameText = '';
+        this.nameText = '';
       },
-      error: (err) => {
-        console.log('Registrazion error: ' + JSON.stringify(err));
-        this.errmessage = err.error.errormessage;
+      error: (error) => {
+        console.log(error);
+        this.airlineerrmessage = error.error.errormessage;
+      }
+    })
+
+  }
+
+  public deleteUser() {
+    this.http.delete(this.us.url + '/user', {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + this.us.get_token(),
+        'cache-control': 'no-cache',
+        'Content-Type':  'application/json',
+      }),
+      body: {
+        id: this.selectedUser
+      }
+    }).subscribe({
+      next: (d) => {
+        console.log(d);
+        this.deleteusererrmessage = undefined;
+        this.getUsers();
+        alert('Utente eliminato con successo');
+      },
+      error: (error) => {
+        console.log(error);
+        this.deleteusererrmessage = error.error.errormessage;
       }
     })
   }
+
 }
